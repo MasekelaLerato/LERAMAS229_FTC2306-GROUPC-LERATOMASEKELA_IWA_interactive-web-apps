e48c4e178b9286f9f85cc0828db210acca3865f5
Masekela Lerato <leratomasekela234@gmail.com>
2023-08-08 17:26:21 +0200

    completed challenge2

---
+++ IWA7/Challenge2/scripts.js
@@ -0,0 +1,9 @@
+const nickname= "";
+const firstname = "";
+
+// emptied the variables so it can default to displaying good morning
+
+
+console.log(`Good Morning ${nickname ||  firstname}!`)
+//removed the single quotes and added '`'
+//added the syntax in one curly bracket
