9681e7a9fc24996e954869c73b670ddb80d06845
Masekela Lerato <leratomasekela234@gmail.com>
2023-08-16 11:53:30 +0200

    changed thr result output

--- IWA7/Challenge3/scripts.js
+++ IWA7/Challenge3/scripts.js
@@ -1,18 +1,18 @@
 const leoName = 'Leo'
 const leoSurname = 'Musvaire        '
-const leoBalance = '9394'
+const leoBalance = '-9394'
 
 const sarahName = 'Sarah    '
 const sarahSurname = 'Kleinhans'
-const sarahBalance = '4582.2'
+const sarahBalance = '-4582.2'
 
 const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = parseInt(leoBalance) + parseInt(sarahBalance)
-const leo = `${leoName}  ${leoSurname.trim()}  (Owed: R ${parseInt(leoBalance).toFixed(2)})`
-const sarah = `${sarahName.trim()}  ${sarahSurname} (Owed: R ${parseInt(sarahBalance).toFixed(2)})`
+const owed = Math.abs(leoBalance) + Math.abs(sarahBalance)
+const leo = `${leoName}  ${leoSurname.trim()}  (Owed: R ${Math.abs(leoBalance).toFixed(2)})`
+const sarah = `${sarahName.trim()}  ${sarahSurname} (Owed: R ${Math.abs(sarahBalance).toFixed(2)})`
 const total = `Total amount owed: R ${owed.toFixed(2)}`
 const result =   leo + '\n' + sarah + '\n' + divider + '\n' +  total + '\n' +   divider
 
