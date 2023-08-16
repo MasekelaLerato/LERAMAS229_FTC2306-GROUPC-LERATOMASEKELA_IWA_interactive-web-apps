5796782c9d8fec4e61034df25dc5b9d788543423
Masekela Lerato <leratomasekela234@gmail.com>
2023-08-16 15:23:05 +0200

    working on the errors

---
+++ IWA10/Challeng1/script.js
@@ -0,0 +1,70 @@
+const currentYear = new Date().getFullYear()
+
+const holidays = {
+    0: {
+        id: 0,
+        name: 'Day of Reconciliation',
+        date: `16 December ${currentYear}`,
+    },
+    1: {
+        id: 1,
+        name: 'Workers Day',
+        date: new Date(`1 April ${currentYear}`),
+    },
+    2: {
+        id: 2,
+        name: 'Day of Goodwill',
+        date: new Date(`26 December ${currentYear}`),
+    },
+    3: {
+        id: 3,
+        name: 'New Year Day',
+        date: new Date(`1 January ${currentYear}`),
+    },
+    4: {
+        id: 4,
+        name: 'Womens Day',
+        date: new Date(`9 August ${currentYear}`),
+    },
+    5: {
+        id: 5,
+        name: 'Heritage Day',
+        date: new Date(`24 September ${currentYear}`),
+    },
+    6: {
+        id: 6,
+        name: 'Christmas Day',
+        date: new Date(`25 December ${currentYear} 13:25`),
+    },
+    7: {
+        id: 7,
+        name: 'Youth Day',
+        date: new Date(`16 June ${currentYear}`),
+    },
+    8: {
+        id: 8,
+        name: 'Human Rights Day',
+        date: new Date(`21 March ${currentYear}`)
+    },
+}
+
+const christmas = 6
+const futureId = 9
+
+// Do not change code above this comment
+
+
+
+console.log( `ID ${futureId} not created yet`)
+
+copied = holidays.christmas
+copied = { name: 'X-mas Day' }
+correctDate = copied.date
+correctDate.hours = 0
+correctDate.minutes = 0
+isEarlier = copied.date < holidays[6].date
+console.log('New date is earlier:', isEarlier)
+if (isEarlier) copied.date = correctDate
+console.log('ID change:', holidays[christmas].id != copied.id || copied.id)
+console.log('Name change:', holidays[christmas].name != copied.name || copied.name)
+console.log('Date change:', holidays[christmas].date != copied.date || copied.date)
