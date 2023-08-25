b56994d8e2bcbef5a5c593974a37708901165aac
Masekela Lerato <leratomasekela234@gmail.com>
2023-08-25 13:01:44 +0200

    completed the 1st challenge

---
+++ IWA14/challenge1/script.js
@@ -0,0 +1,16 @@
+
+// declared the varable using const
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = (Johnhobby) => {
+    console.log(Johnhobby);
+    console.log(Johnhobby);
+  }
+  function Johnhobby () {
+    logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+  }
+  
+  //called the function
+  Johnhobby();
